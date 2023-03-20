848ee99f562596304386fab850b9573357541ddc
Enrico Bolzonello <enribolzonello@gmail.com>
2023-03-20 13:33:01 +0100

    initial setup

---
+++ hw1/G030HW1.py
@@ -0,0 +1,17 @@
+from pyspark import SparkContext, SparkConf
+
+def MR_ApproxTCwithNodeColors(RDD, C):
+    #TODO
+    return
+
+def MR_ApproxTCwithSparkPartitions(RDD):
+    #TODO
+    return
+
+def main():
+    #TODO
+    print("main")
+
+if __name__ == "__main__":
+    main()
+

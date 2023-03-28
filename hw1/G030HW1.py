b3a1ff5ce34d262cee4dac5def1ea2359c52a4d7
Enrico Bolzonello <enribolzonello@gmail.com>
2023-03-28 17:31:52 +0200

    algorithm_1

--- hw1/G030HW1.py
+++ hw1/G030HW1.py
@@ -14,12 +14,19 @@ Usage:
 """
 from pyspark import SparkContext, SparkConf
 from CountTriangles import CountTriangles
-import re,sys,os
-import time
+import re,sys,os,time,random
 
 P=8191
 
-@timeit
+def color_vertices(edge, C, a, b):
+    h_u = ((a * edge[0] + b) % P) % C
+    h_v = ((a * edge[1] + b) % P) % C
+    
+    if h_u == h_v:
+        return (h_u, edge)
+    return (-1, edge)
+
+#@timeit
 def MR_ApproxTCwithNodeColors(RDD, C):
     """First algorithm for Triangle Counting
 
@@ -39,10 +46,18 @@ def MR_ApproxTCwithNodeColors(RDD, C):
 
         Raises:
     """
-    #TODO
-    return
+    a = random.randint(0, P-1)
+    b = random.randint(1, P-1)
 
-@timeit
+    triangle_count = (RDD.map(lambda x : color_vertices(x, C, a, b)) # R1 MAP PHASE
+                        .groupByKey()
+                        .filter(lambda x : x[0] != -1) # R1 SHUFFLE + GROUPING
+                        .flatMap(lambda x : [(0, CountTriangles(list(x[1])))]) # R1 REDUCE PHASE
+                        .reduceByKey(lambda x,y : x+y)) #R2 REDUCE PHASE
+
+    return C**2*triangle_count.collect()[0][1]
+
+#@timeit
 def MR_ApproxTCwithSparkPartitions(RDD, C):
     """Second Algorithm for Triangle Counting
 
@@ -62,14 +77,6 @@ def MR_ApproxTCwithSparkPartitions(RDD, C):
     """
     #TODO
     return
-
-# helper function to remove the directory from the data path
-def rem_dir(s):
-    find_backslash = s.find("/")
-
-    if find_backslash != -1:
-        s = s[find_backslash+1:]
-    return s
 
 # utility function to measure the execution time of a function
 # TODO: for the first function they want the average execution time, rn we are outputting each execution
@@ -95,11 +102,13 @@ def main():
     C = sys.argv[1]
     assert C.isdigit(), "C must be an integer"
     C = int(C)
+    assert C>0, "C must be at least 1"
 
     # parse R parameter
     R = sys.argv[2]
     assert R.isdigit(), "R must be an integer"
     R = int(R)
+    assert R>0, "R must be at least 1"
 
     # parse data_path
     data_path = sys.argv[3]
@@ -108,8 +117,18 @@ def main():
     rawData = sc.textFile(data_path, minPartitions=C)
     edges = rawData.map(lambda x: tuple(map(int, x.split(',')))) # convert the string edges into tuple of int
     edges.repartition(numPartitions=C).cache()
+    numedges = edges.count()
 
-    print(f"File name: {rem_dir(data_path)}\nNumber of partitions C: {C}\nNumber of executions of the second algorithm R: {R}\nNumber of edges: {edges.count()}")
+    _, file_name = os.path.split(data_path)
+    print(f"Dataset = {file_name}\nNumber of Edges = {numedges}\nNumber of Colors = {C}\nNumber of Repetitions = {R}")
+
+    sum_triangles = 0
+    sum_time = 0
+    for i in range(0, R):
+        start_time = time.time()
+        sum_triangles += MR_ApproxTCwithNodeColors(edges, C)
+        sum_time += (time.time() - start_time)*1000
+    print(f"Approximation through node coloring\n- Number of triangles (median over {R} runs) = {int(sum_triangles/R)}\n - Running time (average over {R} runs) = {int(sum_time/R)}")
 
 if __name__ == "__main__":
     main()

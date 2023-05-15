6b2730a1e81502867b308cf06ec61c9801b2ca34
Enrico Bolzonello <enribolzonello@gmail.com>
2023-05-15 13:10:19 +0200

    hw2

--- hw2/G030HW2.py
+++ hw2/G030HW2.py
@@ -15,9 +15,44 @@ Usage:
 from pyspark import SparkContext, SparkConf
 from CountTriangles import CountTriangles
 import sys,os,time,random, statistics
+from collections import defaultdict
 
 P=8191
 DEFAULT_KEY = 0
+
+def countTriangles2(colors_tuple, edges, rand_a, rand_b, p, num_colors):
+    #We assume colors_tuple to be already sorted by increasing colors. Just transform in a list for simplicity
+    colors = list(colors_tuple)  
+    #Create a dictionary for adjacency list
+    neighbors = defaultdict(set)
+    #Creare a dictionary for storing node colors
+    node_colors = dict()
+    for edge in edges:
+
+        u, v = edge
+        node_colors[u]= ((rand_a*u+rand_b)%p)%num_colors
+        node_colors[v]= ((rand_a*v+rand_b)%p)%num_colors
+        neighbors[u].add(v)
+        neighbors[v].add(u)
+
+    # Initialize the triangle count to zero
+    triangle_count = 0
+
+    # Iterate over each vertex in the graph
+    for v in neighbors:
+        # Iterate over each pair of neighbors of v
+        for u in neighbors[v]:
+            if u > v:
+                for w in neighbors[u]:
+                    # If w is also a neighbor of v, then we have a triangle
+                    if w > u and w in neighbors[v]:
+                        # Sort colors by increasing values
+                        triangle_colors = sorted((node_colors[u], node_colors[v], node_colors[w]))
+                        # If triangle has the right colors, count it.
+                        if colors==triangle_colors:
+                            triangle_count += 1
+    # Return the total number of triangles in the graph
+    return triangle_count
 
 # utility function to measure the execution time of a function
 def timeit(f):
@@ -31,9 +66,12 @@ def timeit(f):
         return (ret, run_time)
     return wrap
 
+def hash_function(e, C, a, b):
+    return ((a * e +b) % P) % C
+
 def color_vertices(edge, C, a, b):
-    h_u = ((a * edge[0] + b) % P) % C
-    h_v = ((a * edge[1] + b) % P) % C
+    h_u = hash_function(edge[0], C, a, b)
+    h_v = hash_function(edge[1], C, a, b)
     
     if h_u == h_v:
         return [(h_u, edge)]
@@ -69,13 +107,35 @@ def MR_ApproxTCwithNodeColors(RDD, C):
 
     return (C**2)*triangle_count.take(1)[0][1]
 
-    @timeit
-    def MR_ExactTC(RDD, C):
-        return
+def createpairs(edge, C, a, b):
+    h_u = hash_function(edge[0], C, a, b)
+    h_v = hash_function(edge[1], C, a, b)
+    pairs = []
+    for i in range(0, C):
+        '''s = ""
+        for l in sorted((h_u,h_v,i)):
+            s = s + l
+        pairs.append((l, edge))'''
+        pairs.append((tuple(sorted((h_u,h_v,i))), edge))
+        
+    return pairs
+            
+
+@timeit
+def MR_ExactTC(RDD, C):
+    a = random.randint(0, P-1)
+    b = random.randint(1, P-1)
+
+    triangle_count = (RDD.flatMap(lambda x : createpairs(x, C, a, b))
+                        .groupByKey()
+                        .flatMap(lambda x : [(DEFAULT_KEY, countTriangles2(x[0], x[1], a, b, P, C))])
+                        .reduceByKey(lambda x,y : x+y)
+                    )
+    return triangle_count.take(1)[0][1]
 
 def main():
     # checking number of cmd line parameters
-    assert len(sys.argv) == 4, "Usage: python GO30HW1.py <C> <R> <path/file_name>"
+    assert len(sys.argv) == 5, "Usage: python GO30HW1.py <C> <R> <F> <path/file_name>"
 
 	# Spark setup
     conf = SparkConf().setAppName('G030HW1')
@@ -93,8 +153,14 @@ def main():
     R = int(R)
     assert R>0, "R must be at least 1"
 
+    # parse F parameter
+    F = sys.argv[3]
+    assert F.isdigit(), "F must be a binary value"
+    F = int(F)
+    assert F == 0 or F == 1, "F must be 0 or 1"
+
     # parse data_path
-    data_path = sys.argv[3]
+    data_path = sys.argv[4]
     assert os.path.isfile(data_path), "File or folder not found"
 
     rawData = sc.textFile(data_path, minPartitions=16)
@@ -106,14 +172,24 @@ def main():
 
     sum_time = 0
     runs_triangles = []
-    for _ in range(0, R):
-        triangles, time = MR_ApproxTCwithNodeColors(edges, C)
-        runs_triangles.append(triangles)
-        sum_time += time
+    if F == 0:
+        for _ in range(0, R):
+            triangles, time = MR_ApproxTCwithNodeColors(edges, C)
+            runs_triangles.append(triangles)
+            sum_time += time
     
-    print(f"Approximation through node coloring\n"
-          f"- Number of triangles (median over {R} runs) = {statistics.median(runs_triangles)}\n"
-          f"- Running time (average over {R} runs) = {int(sum_time/R)} ms")
+        print(f"Approximation through node coloring\n"
+            f"- Number of triangles (median over {R} runs) = {statistics.median(runs_triangles)}\n"
+            f"- Running time (average over {R} runs) = {int(sum_time/R)} ms")
+    elif F == 1:
+        for _ in range(0,R):
+            triangles, time = MR_ExactTC(edges, C)
+            sum_time += time
+        
+        print(f"Exact algorithm with node coloring\n"
+            f"- Number of triangles = {triangles}\n"
+            f"- Running time (average over {R} runs) = {int(sum_time/R)} ms")
+
 
 if __name__ == "__main__":
     main()
